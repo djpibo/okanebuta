90dfa762069306c58372ccef2b19aa945fa7f30c
deejay95 <djdj0622@gmail.com>
2024-02-09 16:21:15 +0900

    [ADD] PATCH, DELETE API

--- src/orm.py
+++ src/orm.py
@@ -22,3 +22,12 @@ class ToDo(Base):
             contents=request.contents,
             is_done=request.is_done,
         )
+
+    # done 메소드를 여러 군데서 반복해서 사용한다면 따로 메소드를 빼놓는 것이 효율적
+    def done(self) -> "ToDo":
+        self.is_done = True
+        return self
+
+    def undone(self) -> "ToDo":
+        self.is_done = False
+        return self
--- src/repository.py
+++ src/repository.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.orm import Session
 
 from orm import ToDo
@@ -19,6 +19,18 @@ def get_todo_by_todo_id(session: Session, todo_id: int) -> ToDo | None:
 # JPA Entity 유사
 def create_todo(session: Session, todo: ToDo) -> ToDo:
     session.add(instance=todo)
-    session.commit()  # db save
-    session.refresh(instance=todo)  # reload
+    session.commit()  # db insert
+    session.refresh(instance=todo)  # result reload
     return todo
+
+
+def update_todo(session: Session, todo: ToDo) -> ToDo:
+    session.add(instance=todo)
+    session.commit()  # db update
+    session.refresh(instance=todo)  # result reload
+    return todo
+
+
+def delete_todo(session: Session, todo_id: int) -> None:
+    session.execute(delete(ToDo).where(ToDo.id == todo_id))
+    session.commit()
